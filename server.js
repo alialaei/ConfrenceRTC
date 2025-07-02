c5c52f05834db73652f011e1902ae8938e917572
Ali Alaei <alialaey@gmail.com>
2025-07-02 17:57:52 +0200

    second

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*'
+    origin: ['https://conference.mmup.org'], // frontend domain
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
